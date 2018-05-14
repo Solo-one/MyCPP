d5f7b0819ef28d297ef1e9fe7d1729d5cd91f65a
fangxiaoming01 <fangxiaoming01@baidu.com>
2018-05-14 13:46:50 +0800

    update

--- demo1_4/main.cpp
+++ demo1_4/main.cpp
@@ -15,6 +15,16 @@ TEST(FunTest, HandlesZeroInput) {
     EXPECT_EQ(2, func(1));
 }
 
+TEST(test1, num) {
+    ASSERT_EQ(1, 1);
+    EXPECT_NE(1, 3);
+}
+
+TEST(test2, num) {
+    ASSERT_GE(3, 2);
+    ASSERT_GT(3, 1);
+}
+
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
